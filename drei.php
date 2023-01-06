3ae3eb693e492c80f2247c439f119299733c7806
Lorenz <lorenzbertz@icloud.com>
2023-01-06 07:59:33 +0100

    start

---
+++ drei.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-US">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Today's Date</title>
+    <script>
+        let d = new Date();
+        //alert("Today's date is " + d);
+		//alert("BINGCHILLING");
+    </script>
+</head>
+ 
+<body>
+<form action="emailerfassung.php" method="get">
+
+<p>Ihre E-Mail-Adresse<br>
+<input type="Text" name="email"></p>
+
+<p>Name:<br>
+<input type="Text" name="name"></p>
+
+<input type="Submit" name="" value="speichern">
+</body>
+ 
+ 
+ 
+</html>
